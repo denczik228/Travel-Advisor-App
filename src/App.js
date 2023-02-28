a51a6027195ca48bb502bbfc96715cd4409ee6c0
denczik228 <bernikovden@gmail.com>
2023-02-28 16:09:50 +0200

    debugging/realization search by places in header part

--- src/App.js
+++ src/App.js
@@ -36,20 +36,21 @@ const App = () => {
   }, [rating])
 
   useEffect(() => {
-    setIsLoading(true)
-    getPlacesData( type, bounds.sw, bounds.ne )
-      .then((data) => {
+    if ((bounds.sw && bounds.ne)) {
+      setIsLoading(true);
+      getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
         //console.log(data)
-        setPlaces(data)
-        setFilteredPlaces([])
-        setIsLoading(false)
-    })
-  }, [type, bounds, coordinates]);
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setIsLoading(false);
+      });
+    }
+  }, [type, bounds]);
   
     return (
       <>
         <CssBaseline />
-        <Header />
+        <Header setCoordinates={setCoordinates} />
         <Grid container spacing={3} style={{ width: "100%" }}>
           <Grid item xs={12} md={4}>
             <List
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 import SearchIcon from '@material-ui/icons/Search';
 
 import useStyles from './styles';
 
-const Header = () => {
-    const classes = useStyles();
+const Header = ({ setCoordinates }) => {
+  const classes = useStyles();
+  const [autocomplete, setAutocomplete] = useState({});
 
+  const onLoad = (autoComp) => setAutocomplete(autoComp)
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat()
+    const lng = autocomplete.getPlace().geometry.location.lng()
+  setCoordinates({lat, lng})
+  }
     return (
       <AppBar position="static">
         <Toolbar className={classes.toolbar}>
@@ -18,14 +25,14 @@ const Header = () => {
             <Typography variant="h6" className={classes.title}>
               Ready to explore new places
             </Typography>
-            {/* <Autocomplete> */}
-                        <div className={classes.search}>
-                            <div className={classes.searchIcon}>
-                            <SearchIcon/>
-                            </div>
-                            <InputBase placeholder="Search ..." classes={{root:classes.inputRoot, input:classes.inputInput}}/> 
+            <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                 <SearchIcon/>
+                </div>
+                <InputBase placeholder="Search ..." classes={{root:classes.inputRoot, input:classes.inputInput}}/> 
               </div>
-            {/* </Autocomplete> */}
+            </Autocomplete>
           </Box>
         </Toolbar>
       </AppBar>
